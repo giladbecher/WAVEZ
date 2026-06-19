f6203847b6441cb868785e80e0a7a3e81c28937c
Gilad Becher <giladbecher@gmail.com>
2026-06-19 15:26:30 +0300

    feat: add custom HTML head and public manifest for proper homescreen icon rendering on iOS/Android PWA

---
+++ wavez-mobile/app/+html.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import { ScrollViewStyleReset } from 'expo-router/html';
+
+export default function HTML({ children }) {
+  return (
+    <html lang="he">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
